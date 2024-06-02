f24b060bbd4680848e2de8724c08615475cca4af
Hemant <hmewada74@gmail.com>
2024-06-02 14:31:51 +0530

    screenshot removed

--- controllers/testControllers.js
+++ controllers/testControllers.js
@@ -262,7 +262,7 @@ const scrapeData = async (req, res) => {
 };
 const scrapedData = async (req, res) => {
   try {
-    const savedCompanies = await Company.find({});
+    const savedCompanies = await Company.find({}).select(["-screenshot"]);
     return res.status(200).send({
       status: true,
       message: "All companies",
